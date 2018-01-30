fac1a3ac1f167652ce04ac83a0f42b5e54b25bb6
David Freese <dfreese@stanford.edu>
2018-01-29 19:01:43 -0800

    MAINT: bump version for time cal and bug fix

--- miil/version.py
+++ miil/version.py
@@ -1,7 +1,7 @@
-__version_major__ = '1'
-__version_minor__ = '1'
-__version_patch__ = '0'
-__version__ = '.'.join((
-    __version_major__,
-    __version_minor__,
-    __version_patch__))
+__version_major__ = 1
+__version_minor__ = 2
+__version_patch__ = 1
+__version__ = 'v{0}.{1}.{2}'.format(
+        __version_major__,
+        __version_minor__,
+        __version_patch__)
